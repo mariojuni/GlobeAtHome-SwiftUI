2fa5845572596ce9d4f4edef174c81aba05715d1
Mario Juni <mario.juni@bell.ca>
2021-02-05 21:36:12 +0800

    Initial Commit

---
+++ SwiftUI-CustomView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-CustomView
+//
+//  Created by Mario Juni on 2/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-CustomView/SwiftUI_CustomViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CustomViewApp.swift
+//  SwiftUI-CustomView
+//
+//  Created by Mario Juni on 2/5/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CustomViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
