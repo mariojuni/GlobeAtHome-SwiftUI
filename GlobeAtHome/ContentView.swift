d4f1d11ec1d27006cc8ae77cf85497491b4460e4
Mario Juni <mario.juni@bell.ca>
2021-02-17 21:12:13 +0800

    Renamed project

--- GlobeAtHome/ContentView.swift
+++ GlobeAtHome/ContentView.swift
@@ -13,9 +13,8 @@ struct ContentView: View {
     var body: some View {
         GeometryReader { geometry in
             VStack {
-
                 ZStack {
-                    HomeView()
+                    HomeScreen()
                     
                     ZStack(alignment: .bottom ) {
                         HStack(alignment: .bottom ) {
--- GlobeAtHome/Modules/Home Screen/HomeScreen.swift
+++ GlobeAtHome/Modules/Home Screen/HomeScreen.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct HomeView: View {
+struct HomeScreen: View {
     var body: some View {
         GeometryReader { geometry in
             ScrollView {
@@ -69,6 +69,6 @@ struct HomeView: View {
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        HomeScreen()
     }
 }
